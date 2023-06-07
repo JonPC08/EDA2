dbd996f0edd57b54534bfda220b3425510c732de
guillev8 <guillem.vizcarro01@estudiant.upf.edu>
2023-06-07 17:37:15 +0200

    posts y solicitudes corregidas, lectura de archivos, ordenacion de usuarios,eliminar usuarios, sugerenicas

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include "main.h"
 #define HASH_SIZE 100
-
+//descargar el archivo users.csv y copiar aqui la ruta de acceso propio
+char resourcesPath[256] = "C:\\Users\\guill\\Downloads\\EDAII BUENO (2)\\EDAII BUENO\\resources\\";
+//añadimos usuarios en formato linked list
 void add_user(Usuario** lista, Usuario* usuario_nuevo) {
     if (*lista == NULL) {
         *lista = usuario_nuevo;
@@ -18,8 +20,64 @@ void add_user(Usuario** lista, Usuario* usuario_nuevo) {
     usuario_nuevo->num_soli = 0;
     usuario_nuevo->num_posts = 0;
 }
-
+//algoritmo de ordenacion para hacer una llamada en el metodo de ordenacion alfabetica
+void bubble_sort(Usuario** arr, int n) {
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (strcmp(arr[j]->name, arr[j + 1]->name) > 0) {
+                Usuario* temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+//metodo de muestra de usuarios noraml
 Usuario* print_users(Usuario* lista) {
+    // Copiar la lista de usuarios en un nuevo arreglo
+    int num_usuarios = 0;
+    Usuario* current = lista;
+    while (current != NULL) {
+        num_usuarios++;
+        current = current->siguiente;
+    }
+
+    Usuario** usuarios_arr = malloc(num_usuarios * sizeof(Usuario*));
+    current = lista;
+    int i = 0;
+    while (current != NULL) {
+        usuarios_arr[i] = current;
+        current = current->siguiente;
+        i++;
+    }
+
+    // Ordenar los usuarios por nombre
+    bubble_sort(usuarios_arr, num_usuarios);
+
+    // Imprimir los usuarios ordenados
+    for (i = 0; i < num_usuarios; i++) {
+        printf("------------------------------------------\n");
+        printf("----------- Ficha del usuario %i ------------\n", usuarios_arr[i]->id);
+        printf("Nombre de usuario: %s\n", usuarios_arr[i]->name);
+        printf("Ano de nacimiento: %i\n", usuarios_arr[i]->year_nacimiento);
+        printf("Correo: %s\n", usuarios_arr[i]->correo);
+        printf("Localizacion: %s\n", usuarios_arr[i]->Localizacion);
+        printf("Gustos: ");
+        for (int j = 0; j < usuarios_arr[i]->cantidad_de_gustos; j++) {
+            printf("%s ", usuarios_arr[i]->gustos[j]);
+        }
+        printf("\nID de usuario: %d\n", usuarios_arr[i]->id);
+        printf("------------------------------------------\n");
+    }
+
+    // Liberar la memoria asignada al arreglo
+    free(usuarios_arr);
+
+    return lista;
+}
+//metodo de muestra de usuarios alfabetica
+Usuario* print_users2(Usuario* lista) {
     Usuario* actual_Usuario = lista;
     while (actual_Usuario != NULL) {
         printf("------------------------------------------\n");
@@ -37,9 +95,9 @@ Usuario* print_users(Usuario* lista) {
         actual_Usuario = actual_Usuario->siguiente;
         printf("------------------------------------------\n");
     }
-    return actual_Usuario;
+    return lista;
 }
-
+//busqued de usuario por nombre
 Usuario* search_user(Usuario* head, char* nombre) {
     Usuario* actual = head;
     while (actual != NULL) {
@@ -50,17 +108,16 @@ Usuario* search_user(Usuario* head, char* nombre) {
     }
     return NULL; // if the user is not found, return NULL
 }
+//implementacion de tabla hash
 int hash_function(int id){
     return id % HASH_SIZE;
-}
-int leerArchivo(const char* nombreArchivo, Usuario** lista, int id_count) {
+} //lectura del archivo de usuarios
+int leerArchivo(const char* nombreArchivo, Usuario** lista, int id_count, Usuario** hash_table) {
     FILE* archivo = fopen(nombreArchivo, "r"); // Open the .csv file in read mode
     printf("%s", nombreArchivo);
     if (archivo == NULL) {
         printf("No se pudo abrir el archivo.\n");
-        return -1;
     }
-
     char linea[256];
     while (fgets(linea, sizeof(linea), archivo)) {
         // Parse the line and extract user information
@@ -77,6 +134,7 @@ int leerArchivo(const char* nombreArchivo, Usuario** lista, int id_count) {
 
         // Create a new user and populate the fields
         Usuario* usuario_nuevo = malloc(sizeof(Usuario));
+        usuario_nuevo->num_amigos = 0;
         usuario_nuevo->siguiente = NULL;
         strcpy(usuario_nuevo->name, nombre);
         usuario_nuevo->year_nacimiento = year_nacimiento;
@@ -92,14 +150,18 @@ int leerArchivo(const char* nombreArchivo, Usuario** lista, int id_count) {
                 strcpy(usuario_nuevo->gustos[i], "");
             }
         }
-
         // Add the user to the list
-        add_user(&lista, usuario_nuevo);
+        add_user(lista, usuario_nuevo);
+
+        int hash_index = hash_function(id_count);
+        hash_table[hash_index] = usuario_nuevo;
+
     }
 
     fclose(archivo); // Close the file
     return id_count;
 }
+//añadimos publicaciones en formato stack
 void add_post(Usuario* user, const char* message) {
     // Crear un nuevo post y asignar el mensaje
     post* newPost = (post*)malloc(sizeof(post));
@@ -108,18 +170,19 @@ void add_post(Usuario* user, const char* message) {
     // Establecer el nuevo post como el primer elemento de la pila (nuevo Timeline)
     newPost->siguiente = user->Timeline;
     user->Timeline = newPost;
-
-    printf("El post ha sido agregado con éxito.\n");
+    user->num_posts++;
+    printf("El post ha sido agregado con exito.\n");
 }
-
-void print_posts(Usuario* user) {
+//mostramos las publicaciones del usuario deseado
+void print_posts(char nombre[20], Usuario* lista) {
+    Usuario* user = search_user(lista,nombre);
     printf("Posts de %s:\n", user->name);
     printf("-----------------\n");
 
-    post* currentPost = user->Timeline;
+    post *currentPost = user->Timeline;
     int postCount = 1;
 
-    while (currentPost != NULL) {
+    for (int p = 0; p < user->num_posts; p++) {
         printf("Post %d:\n", postCount);
         printf("%s\n", currentPost->publicacion);
         printf("-----------------\n");
@@ -128,15 +191,16 @@ void print_posts(Usuario* user) {
         postCount++;
     }
 }
+//enviar solicitud de amisad
 void add_friend_request(Usuario* usuarioEnvia, Usuario* usuarioRecibe) {
     if (usuarioEnvia == NULL || usuarioRecibe == NULL) {
-        printf("Error: Usuario no válido\n");
+        printf("Error: Usuario no valido\n");
         return;
     }
 
     // Verificar si ya son amigos
     for (int i = 0; i < usuarioEnvia->num_amigos; i++) {
-        if (usuarioEnvia->amigos[i] == usuarioRecibe) {
+        if (usuarioRecibe->amigos[i] == usuarioEnvia) {
             printf("%s y %s ya son amigos\n", usuarioEnvia->name, usuarioRecibe->name);
             return;
         }
@@ -144,22 +208,22 @@ void add_friend_request(Usuario* usuarioEnvia, Usuario* usuarioRecibe) {
 
     // Verificar si ya hay una solicitud de amistad pendiente
     for (int i = 0; i < usuarioEnvia->num_soli; i++) {
-        if (usuarioEnvia->soli[i] == usuarioRecibe) {
+        if (usuarioRecibe->soli[i] == usuarioEnvia) {
             printf("Ya existe una solicitud de amistad pendiente de %s a %s\n", usuarioEnvia->name, usuarioRecibe->name);
             return;
         }
     }
 
     // Agregar solicitud de amistad
-    if (usuarioEnvia->num_soli < 20) {
-        usuarioEnvia->soli[usuarioEnvia->num_soli] = usuarioRecibe;
-        usuarioEnvia->num_soli++;
+    if (usuarioRecibe->num_soli < 20) {
+        usuarioRecibe->soli[usuarioEnvia->num_soli] = usuarioEnvia;
+        usuarioRecibe->num_soli++;
         printf("Solicitud de amistad enviada de %s a %s\n", usuarioEnvia->name, usuarioRecibe->name);
     } else {
-        printf("Error: No se pueden enviar más solicitudes de amistad\n");
+        printf("Error: No se pueden enviar mas solicitudes de amistad\n");
     }
 }
-
+//gestion de solicitudes, se elimina la solicitud de la lista, en caso de ser aceptadas cada usuario saldrá en la lista de amigos del otro
 void accept_friend_request(Usuario* user) {
     // Mostrar las solicitudes de amistad pendientes
     printf("Solicitudes de amistad pendientes:\n");
@@ -177,21 +241,33 @@ void accept_friend_request(Usuario* user) {
         Usuario* selectedUser = user->soli[opcion - 1];
 
         // Preguntar si desea aceptar o rechazar la solicitud
-        printf("¿Desea aceptar la solicitud de amistad de %s? (1 para aceptar, 0 para rechazar): ", selectedUser->name);
+        printf("Desea aceptar la solicitud de amistad de %s? (1 para aceptar, 0 para rechazar): ", selectedUser->name);
         int decision;
         scanf("%d", &decision);
 
         if (decision == 1) {
             // Agregar al usuario seleccionado como amigo
             if (user->num_amigos >= MAX_AMIGOS) {
-                printf("No se puede aceptar la solicitud. Se ha alcanzado el límite de amigos.\n");
+                printf("No se puede aceptar la solicitud. Se ha alcanzado el limite de amigos.\n");
                 return;
             }
 
             user->amigos[user->num_amigos] = selectedUser;
             user->num_amigos++;
+            //añadir en las dos listas de amigos
+            selectedUser->amigos[selectedUser->num_amigos] = user;
+            selectedUser->num_amigos++;
 
             printf("La solicitud de amistad de %s ha sido aceptada.\n", selectedUser->name);
+            printf("Amigos de %s:\n",user->name);
+            for(int j = 0; j<user->num_amigos;j++){
+                printf("%s, ",user->amigos[j]);
+            }
+            printf("\n");
+            printf("Amigos de %s:\n",selectedUser->name);
+            for(int m = 0; m<selectedUser->num_amigos;m++){
+                printf("%s, ",selectedUser->amigos[m]);
+            }
         } else if (decision == 0) {
             // Eliminar la solicitud
             for (int i = opcion - 1; i < user->num_soli - 1; i++) {
@@ -201,13 +277,13 @@ void accept_friend_request(Usuario* user) {
 
             printf("La solicitud de amistad de %s ha sido rechazada.\n", selectedUser->name);
         } else {
-            printf("Opción no válida.\n");
+            printf("Opcion no valida.\n");
         }
     } else if (opcion != 0) {
-        printf("Opción no válida.\n");
+        printf("Opcion no valida.\n");
     }
 }
-
+//eliminar amigos propios y te eliminas a ti de la lista del otro
 void remove_friend(Usuario* usuario) {
     if (usuario->num_amigos == 0) {
         printf("No tienes amigos para eliminar.\n");
@@ -222,7 +298,7 @@ void remove_friend(Usuario* usuario) {
     }
 
     int opcion;
-    printf("Ingrese el número del amigo que desea eliminar: ");
+    printf("Ingrese el numero del amigo que desea eliminar (0 para salir): ");
     scanf("%d", &opcion);
 
     if (opcion >= 1 && opcion <= usuario->num_amigos) {
@@ -235,16 +311,85 @@ void remove_friend(Usuario* usuario) {
             for (int i = opcion - 1; i < usuario->num_amigos - 1; i++) {
                 usuario->amigos[i] = usuario->amigos[i + 1];
             }
+            for(int j = 0; j<amigo->num_amigos;j++) {
+                Usuario *auxiliar = amigo->amigos[j];
+                if (strcmp(auxiliar->name, usuario->name) == 0);
+                for (int n =j; j< amigo->num_amigos;n++){
+                    amigo->amigos[j] = amigo->amigos[j+1];
+                }
+            }
 
             // Actualizar el número de amigos
             usuario->num_amigos--;
+            amigo->num_amigos--;
         } else {
-            printf("Amigo no válido.\n");
+            printf("Amigo no valido.\n");
         }
     } else {
-        printf("Opción no válida.\n");
+        printf("Opcion no valida.\n");
     }
 }
+//muestra otros usuarios con gustos coincidentes
+void sugerencias(Usuario* usuario) {
+    printf("Sugerencias de amistad para %s:\n", usuario->name);
+
+    // Recorrer la lista de usuarios
+    Usuario* actual = usuario->siguiente;
+    while (actual != NULL) {
+        // Verificar si ya son amigos
+        int es_amigo = 0;
+        for (int i = 0; i < usuario->num_amigos; i++) {
+            if (usuario->amigos[i] == actual) {
+                es_amigo = 1;
+                break;
+            }
+        }
+
+        // Verificar si ya hay una solicitud de amistad pendiente
+        int solicitud_pendiente = 0;
+        for (int i = 0; i < usuario->num_soli; i++) {
+            if (usuario->soli[i] == actual) {
+                solicitud_pendiente = 1;
+                break;
+            }
+        }
+
+        // Verificar si comparten al menos un gusto
+        int gustos_comunes = 0;
+        char gustos_en_comun[MAX_NUM_GUSTOS][MAX_GUSTO_LENGTH];
+        for (int i = 0; i < usuario->cantidad_de_gustos; i++) {
+            for (int j = 0; j < actual->cantidad_de_gustos; j++) {
+                if (strcmp(usuario->gustos[i], actual->gustos[j]) == 0) {
+                    strcpy(gustos_en_comun[gustos_comunes], usuario->gustos[i]);
+                    gustos_comunes++;
+                    break;
+                }
+            }
+        }
+
+        // Si no son amigos, no hay solicitud pendiente y comparten al menos un gusto, mostrar el usuario y ofrecer enviar una solicitud de amistad
+        if (!es_amigo && !solicitud_pendiente && gustos_comunes > 0) {
+            printf("Usuario: %s\n", actual->name);
+            printf("Gustos en comun (%d): ", gustos_comunes);
+            for (int i = 0; i < gustos_comunes; i++) {
+                printf("%s ", gustos_en_comun[i]);
+            }
+            printf("\n");
+            printf("Enviar solicitud de amistad? (si/no): ");
+            char respuesta[2];
+            scanf("%s", respuesta);
+            if (strcmp(respuesta, "si") == 0) {
+                add_friend_request(usuario, actual);
+                printf("Solicitud enviada.\n");
+            }
+        }
+
+        actual = actual->siguiente;
+    }
+
+    printf("Fin de las sugerencias.\n");
+}
+
 
 int main() {
     int option = 0;
@@ -263,7 +408,7 @@ int main() {
                "4. Salir\n");
         printf("Selecciona una de las opciones con un numero del 1 al 4:\n");
         scanf("%i", &option);
-
+        /// 1. Opcion para añadir usuarios. 1.1. Añadir usuario manualmente, 1.2. Añadir usuarios con .csv
         if (option == 1) {
             // Upload a user
             int read_csv;
@@ -303,26 +448,39 @@ int main() {
                 add_user(&lista, usuario_nuevo);
                 int hash_index = hash_function(id_count);
                 hash_table[hash_index] = usuario_nuevo;
-            } else if (read_csv == 2) {
-                char archivo_csv[100], path_csv[256] = "C:\\Users\\guill\\CLionProjects\\EDA2";
+            }
+            else if (read_csv == 2) {
+                char archivo_csv[100];
                 printf("Ingrese el nombre del archivo CSV: \n");
                 scanf("%s", archivo_csv);
-                strcat(path_csv, archivo_csv);
-                leerArchivo(path_csv, &lista, id_count);
+                char csvPath[256];
+                strcpy(csvPath, resourcesPath);
+                strcat(csvPath, archivo_csv);
+                id_count = leerArchivo(csvPath, &lista, id_count, hash_table);
+            }else if (read_csv !=1 && read_csv!=2){
+                break;
             }
         }
+            /// 2. Imprimir todos los usuario guardados en la base de datos.
         else if (option == 2) {
             // Print all existing users
             char respuesta[2];
             printf("Quieres ver los usuarios? (si/no): \n");
             scanf("%s", respuesta);
             if (strcmp(respuesta, "si") == 0) {
-                print_users(lista);
+                printf("Deseas ver los usuarios en orden alfabetico? (si/no): \n");
+                scanf("%s", respuesta);
+                if (strcmp(respuesta, "si") == 0) {
+                    print_users(lista);
+                } else {
+                    print_users2(lista);
+                }
             } else {
                 printf("Volveras al menu.\n");
             }
         }
 
+            /// 3. Buscar un usuario entre todos y iniciar sesion como él.
         else if (option == 3) {
             // Search for a specific user in the list
             // declaramos antes la variable +encontrado para que no haya un problema de memoria al inicalizar en los bloques if, else if
@@ -363,10 +521,13 @@ int main() {
                            "2. Gestionar solicitudes pendientes.\n"
                            "3. Realizar una publicacion.\n"
                            "4. Listar las publicaciones de un usuario.\n"
-                           "5. Salir al menu\n");
+                           "5. Mostrar las sugerencias\n"
+                           "6. Salir al menu\n");
                     int sub_option;
                     printf("Selecciona una de las opciones con un numero del 1 al 5:\n");
                     scanf("%i", &sub_option);
+
+                    /// 3.1. Enviar solicitud de amistad a otro usuario
                     if (sub_option == 1) {
                         // Send a friend request
                         char friend_name[20];
@@ -384,6 +545,7 @@ int main() {
                             printf("El amigo no fue encontrado.\n");
                         }
                     }
+                        /// 3.2. Gestionar las solicitudes pendientes.
                     else if (sub_option == 2) {
                         accept_friend_request(encontrado);
                         printf("Deseas eliminar algun amigo? (si/no) ");
@@ -393,6 +555,7 @@ int main() {
                             remove_friend(encontrado);
                         }
                     }
+                        /// 3.3. Realizar una publicación.
                     else if (sub_option ==3){
                         printf("Introduce aqui tu post (maximo 120 caracteres): ");
                         char publicacion[121]; // 120 caracteres + 1 para el carácter nulo
@@ -401,9 +564,22 @@ int main() {
                         add_post(encontrado, publicacion);
                     }
                     else if (sub_option == 4) {
-                        print_posts(encontrado);
+                        printf("De quien quieres ver los posts? ");
+                        getchar();
+                        char nombreaver[20];
+                        fgets(nombreaver, sizeof(nombreaver), stdin);
+                        nombreaver[strcspn(nombreaver, "\n")] = '\0';
+                        print_posts(nombreaver,lista);
                     }
-                    else if (sub_option == 5) {
+                    else if (sub_option == 5){
+                        printf("Quieres ver las sugerencias? (si/no)");
+                        char mostrar[4];
+                        scanf("%s",mostrar);
+                        if (strcmp(mostrar,"si")==0){
+                            sugerencias(encontrado);
+                        }
+                    }
+                    else if (sub_option == 6) {
                         break;
                     }
                 }
@@ -420,6 +596,6 @@ int main() {
             printf("\n\nLa opcion que has seleccionado no esta disponible.\n");
             printf("Por favor, selecciona una opcion con un numero del 1-5.\n\n");
         }
-}
+    }
     return 0;
 }
--- main.h
+++ main.h
@@ -1,18 +1,28 @@
 #ifndef EDA2_MAIN_H
 #define EDA2_MAIN_H
-#define MAX_AMIGOS 100
 
-#endif //EDA2_MAIN_H//
-typedef struct {
-    char name[20];
+#define MAX_AMIGOS 100
+#define MAX_NAME_LENGTH 20
+#define MAX_EMAIL_LENGTH 30
+#define MAX_LOCATION_LENGTH 20
+#define MAX_NUM_GUSTOS 5
+#define MAX_GUSTO_LENGTH 30
+#define MAX_PASSWORD_LENGTH 20
+#define MAX_POST_LENGTH 121
+
+typedef struct Usuario Usuario;
+typedef struct Post Post;
+
+struct Usuario {
+    char name[MAX_NAME_LENGTH];
     int year_nacimiento;
-    char correo[30];
-    char Localizacion[20];
+    char correo[MAX_EMAIL_LENGTH];
+    char Localizacion[MAX_LOCATION_LENGTH];
     int cantidad_de_gustos;
-    char gustos[5][30];
-    char password[20];
+    char gustos[MAX_NUM_GUSTOS][MAX_GUSTO_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
     int id;
-    struct post* Timeline;
+    struct Post* Timeline;
     struct Usuario* soli[20];
     struct Usuario* head;
     struct Usuario* siguiente;
@@ -20,8 +30,7 @@ typedef struct {
     int num_amigos; // Number of friends
     int num_soli;
     int num_posts;
-} Usuario;
-
+};
 
 typedef struct{
     char publicacion[121];
@@ -29,12 +38,14 @@ typedef struct{
 }post;
 
 void add_user(Usuario** lista, Usuario* usuario_nuevo);
-Usuario* print_users(Usuario *lista);
-Usuario *search_user(Usuario *node, char *nombre);
+Usuario* print_users(Usuario* lista);
+Usuario* search_user(Usuario* node, char* nombre);
 int hash_function(int id);
 void add_post(Usuario* usuario, const char* message);
-void print_posts(Usuario* user);
+void print_posts(char nombre[20], Usuario* lista);
 void add_friend_request(Usuario* usuario, Usuario* amigo);
 void accept_friend_request(Usuario* usuario);
 void remove_friend(Usuario* usuario);
-int leerArchivo(const char* nombreArchivo, Usuario** lista, int id_count);
+int leerArchivo(const char* nombreArchivo, Usuario** lista, int id_count, Usuario** hash_table);
+
+#endif // EDA2_MAIN_H
